1d21fbdaef078bca6e6cc7cfa4e4400713332e88
quoc269 <quoc269@gmail.com>
2022-01-07 15:57:42 +0700

    Initilizing the first app

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React, { Component } from 'react'  
+import { View,StyleSheet} from 'react-native'  
+import {WebView} from 'react-native-webview'  
+  
+export default class App extends Component {  
+    render() {  
+        return (  
+            <View style = {styles.container}>  
+                {/*<WebView 
+                    source={{html: '<h1>Hello javaTpoint</h1>'}} 
+                />*/}  
+             {/*   <WebView 
+                    source={require("./resources/index.html")} 
+                />*/}  
+                <WebView  
+                    source = {{ uri:'https://quoc-pet-shop.netlify.app' }}  
+                />  
+            </View>  
+        )  
+    }  
+}  
+const styles = StyleSheet.create({  
+    container: {  
+        flex: 1,  
+    }  
+}) 
